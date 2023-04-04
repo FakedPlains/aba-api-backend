c4581ea9b00733240670db5a3a146832f9caba22
张富玮 <460293650@qq.com>
2023-04-04 21:40:39 +0800

    创建新数据库表

--- aba-api-gateway/src/main/java/cloud/zfwproject/abaapi/gateway/GatewayApplication.java
+++ aba-api-gateway/src/main/java/cloud/zfwproject/abaapi/gateway/GatewayApplication.java
@@ -4,6 +4,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 46029
@@ -12,7 +13,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @date 2023/3/9 21:41
  */
 @EnableDubbo
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}, scanBasePackages = {"cloud.zfwproject.abaapi.gateway", "cloud.zfwproject.abaapi.common"})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
--- sql/ddl.sql
+++ sql/ddl.sql
@@ -6,7 +6,7 @@ use aba_api;
 
 -- 用户表
 drop table if exists `user`;
-create table if not exists `user`
+create table `user`
 (
     id            bigint auto_increment comment 'id' primary key,
     user_name     varchar(256)                           null comment '用户昵称',
@@ -26,7 +26,7 @@ create table if not exists `user`
 
 -- 接口表
 drop table if exists `interface_info`;
-create table if not exists `interface_info`
+create table `interface_info`
 (
     `id`           bigint unsigned auto_increment comment 'id' primary key,
     `name`         varchar(256) not null comment '接口名称',
@@ -43,7 +43,7 @@ create table if not exists `interface_info`
 
 -- 接口请求参数
 drop table if exists `interface_param`;
-create table if not exists `interface_param`
+create table `interface_param`
 (
     `id`                bigint unsigned auto_increment comment 'id' primary key,
     `interface_info_id` bigint           not null comment '接口 id',
@@ -60,7 +60,8 @@ create table if not exists `interface_param`
 ) comment '接口请求参数信息表';
 
 -- 用户接口关系表
-create table if not exists user_interface_info
+drop table if exists `user_interface_info`;
+create table `user_interface_info`
 (
     `id`                bigint auto_increment comment 'id' primary key,
     `user_id`           bigint                             not null comment '调用用户',
@@ -73,12 +74,54 @@ create table if not exists user_interface_info
     `is_delete`         tinyint  default 0                 not null comment '是否删除'
 ) comment '用户接口关系表';
 
+-- 接口计费信息表
+drop table if exists `interface_charging`;
+create table `interface_charging`
+(
+    `id`                bigint auto_increment comment 'id' primary key,
+    `interface_info_id` bigint                                  not null comment '接口 id',
+    `is_charge`         tinyint       default 0                 not null comment '是否收费 0-免费 1-收费',
+    `price`             decimal(5, 2) default 0                 not null comment '单价（元/次）',
+    `free_count`        int           default 0                 not null comment '免费试用次数',
+    `create_time`       datetime      default CURRENT_TIMESTAMP not null comment '创建时间',
+    `update_time`       datetime      default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    `is_delete`         tinyint       default 0                 not null comment '是否删除'
+) comment '接口计费信息';
+
+-- 用户购买接口表
+drop table if exists `user_interface_order`;
+create table `user_interface_order`
+(
+    `id`                bigint auto_increment comment 'id' primary key,
+    `user_id`           bigint                                   not null comment '用户 id',
+    `interface_info_id` bigint                                   not null comment '接口 id',
+    `order_sn`          char(32)                                 not null comment '订单号',
+    `count`             bigint         default 0                 not null comment '购买次数',
+    `cost`              decimal(19, 4) default 0                 not null comment '花费',
+    `status`            tinyint        default 0                 not null comment '订单状态',
+    `create_time`       datetime       default CURRENT_TIMESTAMP not null comment '创建时间',
+    `update_time`       datetime       default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    `is_delete`         tinyint        default 0                 not null comment '是否删除'
+) comment '用户购买接口表';
+
+-- 用户调用记录表
+drop table if exists `invoke_history`;
+create table `invoke_history`
+(
+    `id`            bigint auto_increment comment 'id' primary key,
+    `invoke_time`   timestamp                          not null comment '调用时间',
+    `invoke_status` tinyint  default 0                 not null comment '调用状态',
+    `create_time`   datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    `update_time`   datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    `is_delete`     tinyint  default 0                 not null comment '是否删除'
+) comment '调用历史记录表';
+
 drop table if exists `dict_type`;
-create table if not exists `dict_type`
+create table `dict_type`
 (
     `id`          bigint auto_increment comment 'id' primary key,
     `name`        varchar(20)                        not null comment '字典类型名称',
-    `description`        varchar(100)                       null comment '字典类型描述',
+    `description` varchar(100)                       null comment '字典类型描述',
     `status`      tinyint                            not null default 0 comment '状态',
     `user_id`     bigint                             not null comment '创建用户',
     `create_time` datetime default CURRENT_TIMESTAMP not null comment '创建时间',
@@ -87,7 +130,7 @@ create table if not exists `dict_type`
 ) comment '字典类型表';
 
 drop table if exists `dict_data`;
-create table if not exists `dict_data`
+create table `dict_data`
 (
     `id`           bigint auto_increment comment 'id' primary key,
     `dict_type_id` bigint                             not null comment '字典类型 id',
